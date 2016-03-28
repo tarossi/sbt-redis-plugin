ffd9bb4608ca07e5529fa3b611f714c5a838ac73
François Monniot <fmonniot@users.noreply.github.com>
2016-03-28 13:18:59 -0700

    Configure travis CI

--- publish/deploy.sh
+++ publish/deploy.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env sh
 
+if [[ ("${TRAVIS_PULL_REQUEST}" == "false") && \
+      ("${TRAVIS_JDK_VERSION}" == "oraclejdk7") && \
+      ("${TRAVIS_TAG}" != "") ]]; then
+
+      echo Deploying to bintray with version $TRAVIS_TAG
+      # Create .bintray/.credentials file
+      mkdir -p ~/.bintray
+      echo \"$(< ./publish/credentials.template)\"" > ~/.bintray/.credentials
+
+#    sbt publish
+
+      rm ~/.bintray/.credentials
+else
+    echo no
+fi
